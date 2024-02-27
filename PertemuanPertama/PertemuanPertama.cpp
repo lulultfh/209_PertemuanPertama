dca5126a8a52f4c3816f4cbe405569bb653f2f3d
lulultfh <punyalulu05@gmail.com>
2024-02-27 15:49:22 +0700

    membuat input variabel panjang

--- PertemuanPertama/PertemuanPertama.cpp
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -22,4 +22,10 @@ int main()
     cout << "Luasnya = " << nLuas;
 
     int panjang, lebar, luas;
+    cout << "Masukan Panjang = ";
+    cin >> panjang;
+    cout << "Masukan Lebar = ";
+    cin >> lebar;
+    luas = panjang * lebar;
+    cout << "Luasnya = " << luas;
 }
